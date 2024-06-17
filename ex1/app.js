7a515c232d3609e085e98fa6c901c6b6c04c4dfa
ArturoAlvarezY <arturoavilamarketing@gmail.com>
2024-06-17 18:16:27 +0200

    finished the ex2

--- ex1/app.js
+++ ex1/app.js
@@ -1,7 +1,7 @@
-//Given that i have a h1
+//Given that i have a h1 and have to show a result in screen
 const title= document.getElementById('myH1')
 
-//then i have to print there Hello Javascript
+//then i have to make a function to print the result
 
 const makeThePrint = () => {
     return title.innerHTML= `Hello Javascript!`
