0565d0e4ff058483d16689c5635c49f22037841b
ArturoAlvarezY <arturoavilamarketing@gmail.com>
2024-06-17 18:04:41 +0200

    make the first exercise

---
+++ ex1/app.js
@@ -0,0 +1,12 @@
+//Given that i have a h1
+const title= document.getElementById('myH1')
+
+//then i have to print there Hello Javascript
+
+const makeThePrint = () => {
+    return title.innerHTML= `Hello Javascript!`
+};
+
+//When i call my function
+
+makeThePrint();
